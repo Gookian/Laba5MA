f15f6444900175f95d00bee174ebf9f5a3ef2e46
Daniil <48077576+Gookian@users.noreply.github.com>
2024-12-17 05:11:40 +0700

    Добавьте файлы проекта.

---
+++ Laba5MA/Controllers/HelloController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Laba5MA.Controllers
+{
+    [Route("api/v1/[controller]")]
+    [ApiController]
+    public class HelloController : ControllerBase
+    {
+        [HttpGet]
+        public async Task<ActionResult<string>> GetAllPersonsAsync()
+        {
+            return Ok("Hello i service!");
+        }
+    }
+}
---
+++ Laba5MA/Startup.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.OpenApi.Models;
+
+namespace Laba5MA
+{
+    public class Startup
+    {
+        public IConfiguration Configuration { get; }
+
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddSwaggerGen(options =>
+            {
+                options.SwaggerDoc("v1", new OpenApiInfo
+                {
+                    Title = "API сервиса управления навыками сотрудников",
+                    Description = "Этот сервис позволяет управлять навыками " +
+                                  "сотрудников, отслеживать их уровень и " +
+                                  "актуализировать информацию о компетенциях " +
+                                  "сотрудников.",
+                    Version = "v1",
+                });
+            });
+
+            services.AddControllers();
+        }
+
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            app.UseSwagger();
+            app.UseSwaggerUI(options =>
+            {
+                options.SwaggerEndpoint("swagger/v1/swagger.json", "Employee Service API");
+                options.RoutePrefix = "";
+            });
+
+            app.UseRouting();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllerRoute(
+                    name: "default",
+                    pattern: "api/v1/{controller=Persons}/{action=Index}/{id?}");
+            });
+        }
+    }
+}
